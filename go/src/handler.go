8fb7b6c149de820442e3924ed859dc6f828cc018
Young Yoon, Choi <43867862+Cyy92@users.noreply.github.com>
2019-10-02 17:21:05 +0900

    Update handler.go

--- go/src/handler.go
+++ go/src/handler.go
@@ -1,6 +1,6 @@
 package main
 
-import sdk "github.com/keti-openfx/openfx-watcher/go/pb"
+import sdk "github.com/keti-openfx/openfx/executor/go/pb"
 
 func Handler(req sdk.Request) string {
 	return string(req.Input)
